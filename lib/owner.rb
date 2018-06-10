878d6dbedb90aa3177be41480db622be78b98369
Leticia Molina <letimolina@gmail.com>
2018-06-10 21:44:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -48,13 +48,14 @@ class Owner
     end
   end
   
-def sell_pets
-  pets.each do |species, animals|
-    animals.each |animal|
-      animal.mood = "nervous"
+  def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
     end
-    animals.clear
   end
-end
+
     
 end
